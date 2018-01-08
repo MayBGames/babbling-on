902a7c094961671a69d4f69aab90834e7555b475
Bryan Maynard <bsgbryan@gmail.com>
2018-01-08 15:24:53 -0600

    Added a clarifying comment
    The x and z constants really should not be calculated using hard coded values.
    The exact same 200 x 200 grid measurement now exists in two places.
    
    That is not ok

--- src/reference_blocks.js
+++ src/reference_blocks.js
@@ -5,6 +5,7 @@ const generate_reference_blocks = (scene) => {
     const box = BABYLON.MeshBuilder.CreateBox(`box${i}`, { }, scene)
 
     // This is kind of crap.
+    // It really shouldn't be hardcoded.
     // I just happen to know that the size of the plane (ground) is 200 x 200
     const x   = Math.random() * 200 - 100
     const z   = Math.random() * 200 - 100
