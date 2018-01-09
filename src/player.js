9cfa29605bd6c90bc7cf6b5d1ba265db8809f9ad
Bryan Maynard <bsgbryan@gmail.com>
2018-01-08 19:16:11 -0600

    Smooth collisions
    Ok, this commit doesn't _exactly_ smooth collisions. What it does instead is simplify the geometry of the player mesh. The player mesh is now a box instead of a cylindar. The box geometry does away with the strange movement behavior of the cylindar mesh; there are only four sides to collide with, so stange subtle movements are not possible as there aren't faces at weird angles to collide with anything.
    
    closes #7

--- src/player.js
+++ src/player.js
@@ -1,5 +1,5 @@
 const create_player = (scene) => {
-  const player = BABYLON.MeshBuilder.CreateCylinder('player', { }, scene)
+  const player = BABYLON.MeshBuilder.CreateBox('player', { height: 2, width: 1, depth: 1 }, scene)
 
   player.material              = new BABYLON.StandardMaterial('player_mat', scene)
   player.material.diffuseColor = new BABYLON.Color3(1, 0.75, 1)
@@ -7,7 +7,7 @@ const create_player = (scene) => {
   player.position        = new BABYLON.Vector3(0, -6, 20)
   player.checkCollisions = true
 
-  player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.CylinderImpostor, { mass: 1, restitution: 0 }, scene)
+  player.physicsImpostor = new BABYLON.PhysicsImpostor(player, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 1, restitution: 0 }, scene)
 
   return player
 }
