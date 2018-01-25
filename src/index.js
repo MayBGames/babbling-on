8e53dcede45cb994a095fff37d045e6c39e23aab
Bryan Maynard <bsgbryan@gmail.com>
2018-01-25 00:23:48 -0600

    Wipe away the old old movement code
    In order to update movement to take rotation into account, I needed to do a complete rewrite of the movement code. Not only does this rewrite close an issue, it also drastically cleans up the `movement.js` code as well as it's interface, which `index.js` uses.
    
    This commit purges all the old code so that the next commit, where all the cool cleaned up stuff is added, is easier to understand and clearer to read.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,3 @@
-import {
-  init_controls,
-  process_pointer_move,
-  process_player_move,
-  MOVE
-} from '/src/movement.js'
-
-import {
-  register_action,
-  KEY_UP,
-  KEY_DOWN
-} from '/src/shared.js'
-
 import { create_camera } from '/src/camera.js'
 import { create_scene  } from '/src/scene.js'
 import { create_player } from '/src/player.js'
@@ -28,7 +15,6 @@ window.addEventListener('DOMContentLoaded', () => {
   const plane  = create_ground(scene)
   const boxes  = generate_reference_blocks(scene)
   
-  init_controls(scene, me)
 
   scene.activeCamera = create_camera(canvas, scene, me)
 
@@ -36,33 +22,4 @@ window.addEventListener('DOMContentLoaded', () => {
   
   canvas.focus()
 
-  engine.runRenderLoop(() => {
-    process_player_move(me)
-    
-    scene.render()
-  })
-
-  let original_canvas_width  = canvas.width
-  let original_canvas_height = canvas.height
-
-  const pointer_move_handler = process_pointer_move(scene.activeCamera, me)
-
-  document.onwebkitfullscreenchange = (event) => {
-    if (engine.isFullscreen === false) {
-      engine.setSize(original_canvas_width, original_canvas_height)
-
-      document.removeEventListener('mousemove', pointer_move_handler, false)
-    } else if (engine.isFullscreen === true) {
-      document.addEventListener('mousemove', pointer_move_handler, false)
-    }
-  }
-
-  document.addEventListener('keydown', (e) => {
-    if (e.which === 13 && engine.isFullscreen === false) {
-      engine.switchFullscreen(true)
-      engine.setSize(screen.width, screen.height)
-
-      canvas.focus()
-    }
-  }, false)
 })
--- src/movement.js
+++ src/movement.js
@@ -4,79 +4,34 @@ import {
   register_action
 } from '/src/shared.js'
 
-const KEYS_DOWN = { }
-const KEYS_UP   = { }
-
-let MOVE_DIVISOR = 6
-let STOP_DELAY   = 50
-
-const MOVE = { Z: 0, Y: 0, X: 0 }
-
 const FORWARD  = 'w'
 const BACKWARD = 's'
 const LEFT     = 'a'
 const RIGHT    = 'd'
 
-const DIRECTION = {
-  [FORWARD]:  (stop) => MOVE.Z = stop === 'STOP' ? 0 : neg(),
-  [BACKWARD]: (stop) => MOVE.Z = stop === 'STOP' ? 0 : pos(),
-  [LEFT]:     (stop) => MOVE.X = stop === 'STOP' ? 0 : pos(),
-  [RIGHT]:    (stop) => MOVE.X = stop === 'STOP' ? 0 : neg()
 }
 
-const compute_direction = (direction, power) => parseFloat(Math[direction](power)) / MOVE_DIVISOR
 
-const pos = () => compute_direction('cos',  1)
-const neg = () => compute_direction('sin', -1)
 
-const init_controls = (scene, me) => {
-  for (let key of Object.keys(DIRECTION)) {
-    register_action(KEY_DOWN, key, scene, () => {
-      if (KEYS_DOWN[key] === undefined) {
-        KEYS_DOWN[key] = Date.now()
 
-        DIRECTION[key]('GO')
-      }
 
-      process_player_move(me)
     })
-    
+
     register_action(KEY_UP, key, scene, () => {
-      if (KEYS_UP[key] === undefined)
-        KEYS_UP[key] = Date.now()
     })
   }
 
-  register_action(KEY_DOWN, ' ', scene, () => {
-    me.physicsImpostor.applyImpulse(new BABYLON.Vector3(0, 8, 0), me.getAbsolutePosition())
-  })
+
+
+
+
 }
 
-const stop_player_move = (key) => {
-  DIRECTION[key]('STOP')
-
-  KEYS_DOWN[key] = undefined
-  KEYS_UP[key]   = undefined
-}
-
-const process_player_move = (me) => {
-  for (let key in KEYS_UP)
-    if (KEYS_UP[key] !== undefined && Date.now() - KEYS_UP[key] > STOP_DELAY)
-      stop_player_move(key)
-
-  me.moveWithCollisions(new BABYLON.Vector3(MOVE.X, MOVE.Y, MOVE.Z))
-}
-
-const process_pointer_move = (camera, me) => {
   return (e) => {
-    me.rotation.y       += parseFloat(e.movementX / screen.width)
-    camera.heightOffset += parseFloat(e.movementY / screen.height)
   }
 }
 
 export {
-  MOVE,
   process_pointer_move,
-  process_player_move,
   init_controls
 }
